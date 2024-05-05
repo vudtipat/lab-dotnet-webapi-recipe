fe67089c8e41c8d569fb637116536c66310e8e4e
vudtipat <vudtipat.saishana@krungsrinimble.com>
2024-05-05 14:57:51 +0700

    create new model

---
+++ DemoWebApi/Models/Recipe.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace DemoWebApi.Models
+{
+	public class Recipe
+	{
+		public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public IEnumerable<string> Direction { get; set; } = null!;
+        public IEnumerable<string> Ingradient { get; set; } = null!;
+        public DateTime Updated { get; set; }
+    }
+}
+
